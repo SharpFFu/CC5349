e2a2d313144f03a0a1129d063a9ac4947d0ab6f3
SharpFFu <37620233+SharpFFu@users.noreply.github.com>
2022-05-26 21:58:57 +0800

    Add files via upload

---
+++ submit_client.sh
@@ -0,0 +1,5 @@
+spark-submit \
+    --master yarn \
+    --deploy-mode client \
+    Comp5349_a2_500215470.py \
+    --output $1
---
+++ submit_cluster.sh
@@ -0,0 +1,5 @@
+spark-submit \
+    --master yarn \
+    --deploy-mode cluster \
+    Comp5349_a2_500215470.py \
+    --output $1

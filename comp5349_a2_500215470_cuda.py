e6b745705cd29f67b025ed36c03df5f4cae34d22
SharpFFu <37620233+SharpFFu@users.noreply.github.com>
2022-05-27 06:49:51 +0800

    Update comp5349_a2_500215470_cuda.py

--- comp5349_a2_500215470_cuda.py
+++ comp5349_a2_500215470_cuda.py
@@ -19,9 +19,6 @@ from pyspark.sql import SparkSession
 spark = SparkSession \
     .builder \
     .appName("COMP5349 A2 500215470") \
-    .config("spark.driver.memory", "16g")\
-    .config("spark.sql.inMemoryColumnarStorage.compressed", "true")\
-    .config("spark.sql.execution.arrow.enabled", "true")\
     .getOrCreate()
 
 """### Load Json file as data frame"""
@@ -77,12 +74,16 @@ Test_data_P_df = Test_data_df.where(col("qas_is_impossible") == False)
 
 Test_data_P_df.show(5)
 
+"""Count the number of contracts that contain poss_record for each question"""
+
 pos_contract_N = Test_data_P_df.groupby("paragraph_context").count().withColumnRenamed("count","positive_contract_num")
 pos_contract_N.show(5)
 
 """Convert to RDD"""
 
 Test_RDD = Test_data_P_df.rdd.map(lambda x:(x[0], x[1], x[2], x[3], x[4])).cache()
+
+"""Cut data to the positive samples and Possible negative samples( stride = 2048 window = 4096)"""
 
 def sample_pos(line):
     stride = 2048
@@ -93,29 +94,43 @@ def sample_pos(line):
     index = 0
     neg_result = []
     for i in range(loop_num):
-        answer_start_of_contract = line[3]
-        answer_end_of_contract = line[3] + len(line[4])
-        if(i* stride <= answer_start_of_contract < i * stride + window):
-          index_end=line[3]%stride+len(line[4])
-          if index_end>=window:
+        c_start = line[3]
+        c_end = line[3] + len(line[4])
+        if (i *stride>c_end) or (i*stride+window <c_start):
+          neg_result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=0, answer_end=0, type_name="possible negative"))  
+        elif(i* stride <= c_start < i * stride + window) :
+          if i * stride + window > c_end:
+           index_end=c_end-i*stride 
+           if index_end>=window:
              index_end=window
-          result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=line[3] % stride, answer_end=index_end, type_name="positive"))
-          index += 1
-        elif (i* stride < answer_start_of_contract <= i * stride + window):
-          index_end=line[3]%stride+len(line[4])
-          if index_end>=window:
+           result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=line[3] % stride, answer_end=index_end, type_name="positive"))
+           index += 1
+          else :
+           index_end=window
+           result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=line[3] % stride, answer_end=index_end, type_name="positive"))
+           index += 1
+        elif ( i * stride + window>i* stride >= c_start):
+          if i * stride + window > c_end:
+           index_end=i*stride-c_end
+           if index_end>=window:
              index_end=window
-          result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=line[3] % stride, answer_end=index_end, type_name="positive"))
-        else:
-          neg_result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=0, answer_end=0, type_name="possible negative"))                
+           result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=0, answer_end=index_end, type_name="positive"))
+           index += 1
+          else :
+           result.append(Row(source=line[0][i * stride: i * stride + window], qas_question=line[1], answer_start=0, answer_end=window, type_name="positive"))  
+           index += 1          
     result.extend(neg_result[:index])
     return result
 
-#pos_Sample_rdd= Test_RDD.flatMap(sample_pos)
 pos_Sample_rdd= Test_RDD.flatMap(sample_pos)
+
+"""RDD -> Dataframe"""
+
 pos_Sample_df= spark.createDataFrame(pos_Sample_rdd).cache()
 
 pos_Sample_df.show(5)
+
+"""Count the number of positive samples for each question. (If the start and end of the answer are not 0, it means positive samples)"""
 
 pos_result = pos_Sample_df.select("source","qas_question","answer_start","answer_end")
 pos_count = pos_Sample_df.where(col("type_name")=="positive").groupBy("qas_question").count()
@@ -124,13 +139,19 @@ pos_result.show(5)
 
 pos_count.show(5)
 
+"""select qas_is_impossible == true"""
+
 Test_data_IM_df = Test_data_df.where(col("qas_is_impossible") == True).join(pos_count,"qas_question")
 
 Test_data_IM_df.show(5)
 
+"""Count the number of Neg samples for each question"""
+
 impos_sample_df = Test_data_IM_df.join(pos_contract_N ,"paragraph_context").cache()
 
 impos_sample_df.show(5)
+
+"""Calculate the number of Impossible negative samples"""
 
 def sample_impos_count(count, positive_contract_count):
   caclunumber= float(count / positive_contract_count)
@@ -139,15 +160,21 @@ def sample_impos_count(count, positive_contract_count):
     result+=1
   return result
 
+"""udf function to label the ne sample"""
+
 udf_fc = udf(lambda x,y:sample_impos_count(x,y))
 
 impos_sample_df = impos_sample_df.withColumn("impossible_count",udf_fc(col("count"),col("positive_contract_num"))).select("*",round("impossible_count")).withColumnRenamed("round(impossible_count, 0)","impossible_count_result")
 
-impos_sample_df  = impos_sample_df .select("paragraph_context","qas_question","qas_is_impossible","answer_start","answer_text","impossible_count_result")
+impos_sample_df = impos_sample_df.select("paragraph_context","qas_question","qas_is_impossible","answer_start","answer_text","impossible_count_result")
 
-impos_sample_df .show(5)
+impos_sample_df.show(5)
 
-impos_sample_rdd = impos_sample_df .rdd.map(lambda x:(x[0], x[1], x[2], x[3], x[4], x[5]))
+"""DataFrame->RDD"""
+
+impos_sample_rdd = impos_sample_df.rdd.map(lambda x:(x[0], x[1], x[2], x[3], x[4], x[5]))
+
+"""Cut data tonegative samples( stride = 2048 window = 4096)"""
 
 def sample_impos(line):
   stride = 2048
@@ -163,13 +190,33 @@ def sample_impos(line):
       result.append(Row(source=line[1][i * stride: i * stride + window_size], question=line[0], answer_start=0, answer_end=0))
   return result[:source_number]
 
-impos_sample_rdd = impos_sample_rdd.flatMap(sample_impos)
+Ne_rdd = impos_sample_rdd.flatMap(sample_impos)
 
-impos_result = spark.createDataFrame(impos_sample_rdd).cache()
-result = pos_result.union(impos_result)
+"""RDD->Dataframe"""
 
-result.show()
+Ne_result = spark.createDataFrame(Ne_rdd).cache()
 
-result.count()
+"""Combine Neg,Pos and Pos Neg result"""
 
-result.write.json("output_cuda.json")
+F_result = pos_result.union(Ne_result)
+
+"""Change colume name "qas_question " to "qusetion""""
+
+F_result = F_result.withColumnRenamed("qas_question","question")
+
+"""Final result"""
+
+F_result.show()
+
+"""count result"""
+
+F_result.count()
+
+"""Output json"""
+
+F_result.write.json("output_CUDA.json")
+
+
+
+
+
